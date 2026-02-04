53c33727fa04000ba24be6ff472ec1b7e2ab5192
Samy Juma <info@samjuma.co.ke>
2026-02-04 21:58:50 +0300

    Ensure DetailView is used for library detail

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic.detail import DetailView
+from django.views.generic import DetailView
+from .models import Book
+from .models import Library
 
-from .models import Book, Library
 
-
-# Function-based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {
-        "books": books
-    })
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
-# Class-based view using DetailView
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
