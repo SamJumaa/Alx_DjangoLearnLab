bad01519072d4ed7288714ff32f53de2659d19c2
Samy Juma <info@samjuma.co.ke>
2026-02-25 23:04:25 +0300

    Fix CustomUser admin registration and checker issues

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
 
-
-@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
     list_filter = ("author", "publication_year")
     search_fields = ("title", "author")
 
+admin.site.register(Book, BookAdmin)
 
+class CustomUserAdmin(UserAdmin):
+    pass
+
+admin.site.register(CustomUser, CustomUserAdmin)
